482c41df260e19d7a63a9866a56446569d0dcc2f
Leticia Andrade <leticiaae@hotmail.com.br>
2022-09-25 19:26:35 -0300

    Conversão de tipos primitivos numericos

---
+++ fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+    
+    public static void main(String[] args) {
+        
+        double a = 1; // implícita
+        System.out.println(a);
+
+        float b = (float) 1.777788888; // explícita (CAST)
+        System.out.println(b);
+
+        int c = 4;
+        byte d = (byte) c; // explícita (CAST)
+        System.out.println(d);
+
+        double e = 1.3;
+        int f = (int) e; // explícita (CAST)
+        System.out.println(f);
+
+        
+    }
+}
