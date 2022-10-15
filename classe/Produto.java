62666ebd6bee8af0138ae3def7a617e2cf7f948f
Leticia Andrade <leticiaae@hotmail.com.br>
2022-10-15 12:52:07 -0300

    Criando dois métodos na classe produto

--- classe/Produto.java
+++ classe/Produto.java
@@ -6,5 +6,14 @@ public class Produto {
     double preco;
     double desconto;
 
-    
+    double precoComDesconto() {
+    double resultado = preco - desconto;
+    return resultado;
+   } 
+
+   
+    double precoComDesconto(double descontoDoGerente) {
+    double resultado = preco - desconto + descontoDoGerente;
+    return resultado;
+   } 
 }
