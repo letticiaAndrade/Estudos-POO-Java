bdb35134a8e510e65c8da399fd5e3a4a8664b680
Leticia Andrade <leticiaae@hotmail.com.br>
2022-11-03 22:44:33 -0300

    Estudo sobre o  "static" em atributos e metodos

---
+++ classe/AreaCirc.java
@@ -0,0 +1,21 @@
+package classe;
+
+public class AreaCirc {
+    
+    double raio;
+    // o atributo com o termo "static" não se associa mais a instancia/objeto e sim a classe 
+    // o final static ou static final é uma constante e não mais variável
+    static double pi = 3.14;
+
+    AreaCirc (double raioInicial) {
+        
+        raio = raioInicial;
+    }
+
+    double area () {
+        return pi * Math.pow(raio, 2);
+    }
+    static double area (double raio) {
+        return pi * Math.pow(raio, 2);
+    }
+}
