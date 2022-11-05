7f34a77614561857708582f89802398f16b1e178
Leticia Andrade <leticiaae@hotmail.com.br>
2022-11-05 11:24:46 -0300

    Valor de tipos primitivos e referência de objetos

--- classe/Data.java
+++ classe/Data.java
@@ -7,6 +7,14 @@ public class Data {
     int ano;
     int dia;
     
+    public Data () {
+
+    }
+
+    public Data(int dia, int mes, int ano) {
+
+    }
+
     String obterDataFormatada() {
         return String.format(" %d / %d / %d", dia,mes,ano);
     }
